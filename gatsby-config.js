02f2f91e59618574b443afa9baa6e5f98dc70cd4
Andreas Sahle <andreas.sahle@wunder.io>
2018-01-15 12:33:49 +0100

    prepare gh pages deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Colorado`,
   },
+  pathPrefix: "/colorado-frontend",
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
